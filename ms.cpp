144589a1068b8a8b4a084c0bc3792153c59daa1b
Christopher Palileo <cpalileo@gmail.com>
2023-11-20 22:20:47 -0800

    dice rolling class working

--- ms.cpp
+++ ms.cpp
@@ -15,9 +15,29 @@ public:
         Roll2 = 0;
         TotalRoll = 0;
     }
+
+    int RollDie() {
+        return rand() % 6 + 1;
+    }
+
+    int RollDice() {
+        Roll1 = RollDie();
+        Roll2 = RollDie();
+        TotalRoll = Roll1 + Roll2;
+    }
+
+    int getTotalRoll() const {
+        return TotalRoll;
+    }
+
 };
 
 
-main() {
+int main() {
+    Dice dice;
+    int totalRoll;
+
+    dice.RollDice();
+    cout << "You rolled " << dice.getTotalRoll() << endl;
     return 0;
 };
